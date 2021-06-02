2713dff6181e08b8a6fc8a28c33d1d63698d8b5e
SUSHIL KUMAR <Sushilkumar168141@gmail.com>
2021-06-02 15:14:41 +0530

    initial

---
+++ DateTime/index.js
@@ -0,0 +1,8 @@
+var d = new Date();
+var date = d.getDate();
+var time = d.getHours();
+var day = d.getDay();
+
+document.write("Date is : " + date + "<br>");
+document.write("Time is : " + time + "<br>");
+document.write("Day is : " + day + "<br>");
